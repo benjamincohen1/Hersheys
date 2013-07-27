59fb1d3fb8370719c227f20744e4e38c09e1d324
Vijay Sridhar <vijaysridhar@Vijay-Sridhars-MacBook-Pro.local>
2013-07-27 17:33:31 -0400

    basic money working

--- Hershey's/ViewController.h
+++ Hershey's/ViewController.h
@@ -8,11 +8,15 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <UITextFieldDelegate> {
+    NSString *theUsername, *thePassword;
+}
 
 @property (weak, nonatomic) IBOutlet UILabel *pointsLabel;
 @property (weak, nonatomic) IBOutlet UIButton *retrievePTS;
 @property (weak, nonatomic) IBOutlet UIButton *removePTS;
 @property (weak, nonatomic) IBOutlet UIButton *addPTS;
+@property (weak, nonatomic) IBOutlet UITextField *username;
+@property (weak, nonatomic) IBOutlet UITextField *password;
 
 @end
--- Hershey's/ViewController.m
+++ Hershey's/ViewController.m
@@ -13,25 +13,96 @@
 @end
 
 @implementation ViewController
-
+@synthesize username, password;
 - (void)viewDidLoad
 {
+    username.delegate = self;
+    password.delegate = self;
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
 - (IBAction)addPoints:(id)sender {
+    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:[NSURL URLWithString:@"http://10.224.41.14:5000/money/add"]];
     
+    //set HTTP Method
+    [request setHTTPMethod:@"POST"];
+    
+    //Implement request_body for send request here username and password set into the body.
+    NSString *request_body = [NSString stringWithFormat:@"Username=%@&Ammount=10", theUsername];
+    //set request body into HTTPBody.
+    [request setHTTPBody:[request_body dataUsingEncoding:NSUTF8StringEncoding]];
+    
+    //set request url to the NSURLConnection
+    NSURLConnection *theConnection = [[NSURLConnection alloc] initWithRequest:request delegate:self];
+    [theConnection start];
 }
 
 - (IBAction)removePoints:(id)sender {
+    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:[NSURL URLWithString:@"http://10.224.41.14:5000/money/remove"]];
     
+    //set HTTP Method
+    [request setHTTPMethod:@"POST"];
+    
+    //Implement request_body for send request here username and password set into the body.
+    NSString *request_body = [NSString stringWithFormat:@"Username=%@&Ammount=10", theUsername];
+    //set request body into HTTPBody.
+    [request setHTTPBody:[request_body dataUsingEncoding:NSUTF8StringEncoding]];
+    
+    //set request url to the NSURLConnection
+    NSURLConnection *theConnection = [[NSURLConnection alloc] initWithRequest:request delegate:self];
+    [theConnection start];
 }
 
 - (IBAction)retrievePoints:(id)sender {
+    if (theUsername.length < 4) {
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Type in your username!"
+                                                        message:@"Press OK to submit your data!"
+                                                       delegate:nil
+                                              cancelButtonTitle:@"OK"
+                                              otherButtonTitles: nil];
+        [alert show];
+    } else {
+        NSLog(@"Hey");
+        NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:[NSURL URLWithString:@"http://10.224.41.14:5000/users/money"]];
+        
+        //set HTTP Method
+        [request setHTTPMethod:@"POST"];
+        
+        //Implement request_body for send request here username and password set into the body.
+        NSString *request_body = [NSString stringWithFormat:@"Username=%@&Password=%@", theUsername, thePassword];
+        //set request body into HTTPBody.
+        [request setHTTPBody:[request_body dataUsingEncoding:NSUTF8StringEncoding]];
+        
+        //set request url to the NSURLConnection
+        NSURLConnection *theConnection = [[NSURLConnection alloc] initWithRequest:request delegate:self];
+        [theConnection start];
+    }
     
 }
 
+-(void)connectionDidFinishLoading:(NSURLConnection *)connection
+{
+    NSLog(@"Success!");
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    NSLog(@"This is the description: %@", textField.placeholder);
+    NSString *whichText = [textField.placeholder stringByReplacingOccurrencesOfString:@"description: " withString:@""];
+    if ([whichText isEqualToString:@"username"]) {
+        theUsername = textField.text;
+        [self retrievePoints:self];
+    } else if ([whichText isEqualToString:@"password"]) {
+        thePassword = textField.text;
+    }
+    return YES;
+}
+
+- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data
+{
+    NSLog(@"heres data:%@", [NSString stringWithUTF8String:[data bytes]]);
+}
 
 - (void)didReceiveMemoryWarning
 {
